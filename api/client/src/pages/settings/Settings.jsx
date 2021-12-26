4f64f3e657dedd6d378f9a0f4f2755ff3e057025
vishnumoger <vishnumoger@gmail.com>
2021-12-26 20:28:47 +0530

    images issues

--- api/client/src/pages/settings/Settings.jsx
+++ api/client/src/pages/settings/Settings.jsx
@@ -12,7 +12,7 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "https://gajanana-youth-club-belke.herokuapp.com/api/images/"
+  const PF = "https://gajanana-youth-club-belke.herokuapp.com/images/"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
