0ed6f21358d99faa010d22b60588b9077b883d2a
vishnumoger <vishnumoger@gmail.com>
2021-12-26 18:32:19 +0530

    db isssues

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -14,7 +14,11 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    res.status(200).json(user);
+    //res.status(200).json(user);
+    res.status(200).json({
+			status: 'succes',
+			data: user,
+		});
   } catch (err) {
     res.status(500).json(err);
   }
