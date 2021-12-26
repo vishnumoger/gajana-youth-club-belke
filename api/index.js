8812063f49123ddf6e062c837aeb7ae17675663e
vishnumoger <vishnumoger@gmail.com>
2021-12-26 18:36:30 +0530

    db error

--- api/index.js
+++ api/index.js
@@ -15,10 +15,9 @@ app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose
   .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:true
+    useCreateIndex: true, 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
